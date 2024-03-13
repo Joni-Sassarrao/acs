30ba0ee737a3daa9d69bdc23cb3f9a4e01fe6ec0
Johnny <jp.sdc006@gmail.com>
2024-03-13 16:39:38 -0300

    acDom

--- acDom/script.js
+++ acDom/script.js
@@ -1,12 +1,44 @@
-const form = document.querySelector('form');
-form.addEventListener('submit', function(event) {
-    event.preventDefault();
+const body = document.body;
 
-    const nome = form.nome.value;
-    const email = form.email.value;
-    const mensagem = form.mensagem.value;
+function banner(){
+    let header = document.createElement('header');
+    let textoHeader = document.createElement('p');
+    textoHeader.innerHTML = "Bem-vindo ao meu site professional";
 
-    console.log('Nome:', nome);
-    console.log('Email:', email);
-    console.log('Mensagem:', mensagem);
-});
+    body.appendChild(header);
+    header.append(textoHeader);
+}
+
+banner();
+
+function apresentacao(){
+    let containerDiv = document.createElement('div');
+    let h2_sozinho = document.createElement('h2');
+    h2_sozinho.innerHTML = "Apresentação Pessoal"
+
+    body.appendChild(containerDiv);
+    containerDiv.append(h2_sozinho);
+
+    containerDiv.classList('container');
+    h2_sozinho.classList('sozinho');
+
+    let imagensDiv = document.createElement('div');
+    let h2Cocchi = document.createElement('h2')
+    h2Cocchi.innerHTML = "Gustavo Estevam Cocchi"
+
+    containerDiv.appendChild(imagensDiv);
+    imagensDiv.appendChild(h2Cocchi);
+
+    
+
+    
+
+
+
+    let imagem_cocchi = document.createElement('img');
+    imagem_cocchi.src = 'imgs/cocchi.png'
+}
+
+apresentacao();
+//h2.setAttribute('type', 'text')
+//h2.classList.add('')
